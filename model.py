9234bcced878c838807bb3bff11ac0a481f3fa14
CalciumNitrade <70808713+CalciumNitrade@users.noreply.github.com>
2024-08-19 18:03:13 -0500

    Update model.py

--- model.py
+++ model.py
@@ -1,35 +1,37 @@
 import torch
 import torch.nn as nn
 
-class Text2Energy(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim, dropout=0.3):
-        super(Text2Energy, self).__init__()
+class Text2Everything(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, dropout=0.0):
+        super(HamiltonSolverTriple, self).__init__()
 
         self.layer1 = nn.Sequential(
             nn.Linear(input_dim, hidden_dim // 2),
             nn.ReLU(),
             nn.Dropout(dropout),
+
             nn.Linear(hidden_dim // 2, hidden_dim // 4),
             nn.ReLU(),
             nn.Dropout(dropout),
         )
         self.layer2 = nn.Sequential(
-            nn.Linear(hidden_dim // 4 + 1, hidden_dim // 8),
+            nn.Linear(hidden_dim // 4 + 3, hidden_dim // 8),
             nn.ReLU(),
             nn.Dropout(dropout),
+
             nn.Linear(hidden_dim // 8, hidden_dim // 16),
             nn.ReLU(),
             nn.Dropout(dropout),
         )
         self.layer3 = nn.Sequential(
-            nn.Linear(hidden_dim // 16 + 1, output_dim),
+            nn.Linear(hidden_dim // 16 + 3, output_dim),
         )
 
-    def forward(self, x, mass_number):
+    def forward(self, x, a, b, c):
         x = self.layer1(x)
-        x = torch.cat((x, mass_number), dim=1)
+        x = torch.concat((x, a, b, c), dim=1)
         x = self.layer2(x)
-        x = torch.cat((x, mass_number), dim=1)
+        x = torch.concat((x, a, b, c), dim=1)
         x = self.layer3(x)
 
         return x
